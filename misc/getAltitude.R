56836a85d57759dd5072d344084466d1828175fb
HannesOberreiter <hoberreiter@gmail.com>
2021-06-14 16:01:58 +0200

    cleaning output

--- misc/getAltitude.R
+++ misc/getAltitude.R
@@ -21,15 +21,16 @@ source("partials/header.R")
 # Code --------------------------------------------------------------------
 # remove empty ones if there are any
 dfCache <- dfData %>% drop_na(longitude, latitude)
-dfCache <- dfCache %>% filter(year == "18/19")
+dfCache <- dfCache %>% filter(year == "20/21")
+
 # select start and end number of row
 # please be careful with hard limit of API for altitude, max. 1_000 per hour and 10_000 per day
 paste("Max rows in our dataframe: ", nrow(dfCache))
 
 # Start and End Row we want to get elevation
-rowStart <- 26
-rowEnd <- 1467
-# rowEnd    <- nrow(dfCache)
+rowStart <- 788
+# rowEnd <- 1406
+rowEnd <- nrow(dfCache)
 
 # Create a Named Vector with IDS
 vIDs <- dfCache$id[rowStart:rowEnd]
@@ -41,13 +42,34 @@ for (i in rowStart:rowEnd) {
   lElevation <- GNsrtm3(lat = dfCache[i, "latitude"], lng = dfCache[i, "longitude"])$srtm3
   print(paste("ID:", lID, " Elevation: ", lElevation))
   vValues[lID] <- lElevation
-  Sys.sleep(4) # we prevent overuse (1k : 1 hour)
+  Sys.sleep(2) # we prevent overuse (1k : 1 hour)
 }
 print("------RESULTS--------")
 vValues
 print("---------------------")
 # save to csv
-write_excel_csv2(
-  as_tibble(vValues, rownames = "id"),
-  file = glue("{here()}/output/altitude.csv")
-)
+as_tibble(vValues) %>%
+  tibble::add_column(id = names(vValues)) %>%
+  write_excel_csv2(
+    .,
+    file = glue("{here()}/output/altitude.csv")
+  )
+
+
+as_tibble(vValues) %>%
+  tibble::add_column(id = names(vValues))
+
+length(vValues[!is.na(vValues)])
+
+vValues
+
+dfCache %>% filter(id == "6657-20/21")
+dfCache %>% filter(id == "7806-20/21")
+
+dfCache <- dfCache %>%
+  mutate(
+    longitude = ifelse(id == "6657-20/21", 10.9016903, longitude)
+  ) %>%
+  mutate(
+    longitude = ifelse(id == "7806-20/21", 14.8083347307326, longitude)
+  )
