1ca9f3bae5db04068f887247c5346ab29b58d1ef
HannesOberreiter <hoberreiter@gmail.com>
2021-07-13 11:27:05 +0200

    cleanup :broom:

--- playground.R
+++ playground.R
@@ -1,36 +1,3 @@
-# Numer of Participants longitudal ----------------------------------------
-dfData %>%
-  drop_na(contact) %>%
-  group_by(year, contact) %>%
-  summarise(
-    contact = contact[[1]]
-  ) %>%
-  ungroup() %>%
-  count(contact, name = "times") %>%
-  group_by(times) %>%
-  summarise(
-    n = n()
-  )
-
-x <- dfData %>%
-  drop_na(contact) %>%
-  group_by(year, contact) %>%
-  summarise(
-    hives_winter = hives_winter[[1]],
-    hives_lost_e = hives_lost_e[[1]],
-    hives_spring_e = hives_spring_e[[1]],
-    contact = contact[[1]],
-    global = "1"
-  ) %>%
-  ungroup() %>%
-  add_count(contact, sort = T) %>%
-  filter(n == 4) %>%
-  fGlmNullModel("global")
-x
-dfData %>%
-  fGlmNullModel("global")
-
-
 # Operation Size & Operational Faktors
 dfData %>%
   filter(year == "19/20") %>%
@@ -267,3 +234,5 @@ dt_model_info <- test %>% # create combinations
 
 # access model info
 dt_model_info %>% unnest(performance)
+
+# Other Stuff ------
